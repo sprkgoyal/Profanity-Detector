f05d179f1c3c78bdbb52956c64670776f9b43c21
Rohit Goyal <46754468+sprkgoyal@users.noreply.github.com>
2020-08-03 23:00:35 +0530

    Added Two more commands

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
 import os
 from collections import defaultdict
+from datetime import datetime, timedelta
 from discord.ext import commands
+from math import ceil, floor
 
-count_abuse = defaultdict(lambda : 0)
+count_abuse = defaultdict(lambda : [0, datetime.now()])
 token = os.environ["BOT_TOKEN"]
+time_limit = 60
 
 client = commands.Bot(command_prefix = '.')
 abuse = set()
@@ -41,24 +44,42 @@ async def remove(ctx, *, word):
         abuse_file.write(f"{word}\n")
     abuse_file.close()
 
+@client.command()
+async def set(ctx, *, minutes):
+    global time_limit
+    time_limit = int(minutes)
+    await ctx.send(f"Time Limit has been changed to {minutes} minutes")
+
 @client.event
 async def on_message(message : discord.Message):
+    global time_limit
     if message.author == client.user:
         return
 
-    if message.content.startswith(".add") or message.content.startswith(".remove"):
+    if message.content.startswith(".add") or message.content.startswith(".remove") or message.content.startswith(".set"):
         if "admin" in [mem.name.lower() for mem in message.author.roles]:
             await client.process_commands(message)
         else:
             await message.channel.send("Only Admins can use this command")
         return
 
+    if message.content == ".remaining":
+        cur = datetime.now()
+        prev = count_abuse[message.author][1]
+        if cur-prev < timedelta(minutes=time_limit) and count_abuse[message.author][0] > 0:
+            await message.channel.send(f"{message.author} has to wait {floor(time_limit - (cur - prev).total_seconds()/60)} minutes and {ceil(60 - (cur - prev).total_seconds()%60)} seconds until your count resets")
+        else:
+            await message.channel.send("You are free to go man")
+        return
+
     if message.content == ".help":
         await message.channel.send("""Hello there, and welcome to the help section of Abuse Detector
 
-There are only two commands available here and each command starts with '.' also called period
+There are only four commands available here and each command starts with '.' also called period
 1) add abusive_word, this adds abusive_word in the abuse dictionary for future detection
-2) remove word, this removes a word from the abuse dictionary""")
+2) remove word, this removes a word from the abuse dictionary
+3) set minutes, sets the count of abuses to zero after 'minutes' minutes from his/her last abuse
+4) remaining, tells us how much time is left until your count resets""")
         return
 
     words = message.content.split()
@@ -73,11 +94,15 @@ There are only two commands available here and each command starts with '.' also
 
     for word in new_words:
         if word.lower() in abuse:
+            cur = datetime.now()
+            if cur - count_abuse[message.author][1] >= timedelta(minutes=time_limit):
+                count_abuse[message.author][0] = 0
+            count_abuse[message.author][1] = datetime.now()
             await message.channel.send(f"{message.author}, your message contains abusive word(s)")
-            count_abuse[message.author] += 1
-            if count_abuse[message.author] == 1:
+            count_abuse[message.author][0] += 1
+            if count_abuse[message.author][0] == 1:
                 await message.channel.send(f"This is your first warning, if you abuse two more times then you will be kicked from this channel")
-            elif count_abuse[message.author] == 2:
+            elif count_abuse[message.author][0] == 2:
                 await message.channel.send(f"This is your second warning, if you abuse one more time then you will be kicked from this channel")
             else:
                 await message.channel.send(f"That's it, you crossed the limit so you are kicked from the server")
@@ -85,7 +110,7 @@ There are only two commands available here and each command starts with '.' also
                     await message.channel.guild.kick(message.author, reason = "Using Abusive Language")
                 except:
                     await message.channel.send("I can't kick you right now, but you will be kicked shortly...")
-                count_abuse[message.author] = 0
+                count_abuse[message.author][0] = 0
             return
 
 client.run(token)
